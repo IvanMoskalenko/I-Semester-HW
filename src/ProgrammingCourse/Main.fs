85aeff18578a36f4328e20cc6f55c3fb8e87a5b6
Ivan Moskalenko <ivon.klass@yandex.ru>
2020-09-18 23:02:35 +0300

    First and second tasks were added

--- src/ProgrammingCourse/Main.fs
+++ src/ProgrammingCourse/Main.fs
@@ -1,88 +1,62 @@
 namespace ProgrammingCourse
 
-open System.Reflection
-
-module AssemblyInfo =
-
-    let metaDataValue (mda: AssemblyMetadataAttribute) = mda.Value
-
-    let getMetaDataAttribute (assembly: Assembly) key =
-        assembly.GetCustomAttributes(typedefof<AssemblyMetadataAttribute>)
-        |> Seq.cast<AssemblyMetadataAttribute>
-        |> Seq.find (fun x -> x.Key = key)
-
-    let getReleaseDate assembly =
-        "ReleaseDate"
-        |> getMetaDataAttribute assembly
-        |> metaDataValue
-
-    let getGitHash assembly =
-        "GitHash"
-        |> getMetaDataAttribute assembly
-        |> metaDataValue
-
-    let getVersion assembly =
-        "AssemblyVersion"
-        |> getMetaDataAttribute assembly
-        |> metaDataValue
-
-    let assembly = lazy (Assembly.GetEntryAssembly())
-
-    let printVersion() =
-        let version = assembly.Force().GetName().Version
-        printfn "%A" version
-
-    let printInfo() =
-        let assembly = assembly.Force()
-        let name = assembly.GetName()
-        let version = assembly.GetName().Version
-        let releaseDate = getReleaseDate assembly
-        let githash = getGitHash assembly
-        printfn "%s - %A - %s - %s" name.Name version releaseDate githash
-
-module Say =
-    open System
-
-    let nothing name = name |> ignore
-
-    let hello name = sprintf "Hello %s" name
-
-    let colorizeIn color str =
-        let oldColor = Console.ForegroundColor
-        Console.ForegroundColor <- (Enum.Parse(typedefof<ConsoleColor>, color) :?> ConsoleColor)
-        printfn "%s" str
-        Console.ForegroundColor <- oldColor
-
 module Main =
     open Argu
-
+    open System
     type CLIArguments =
-        | Info
-        | Version
-        | Favorite_Color of string // Look in App.config
-        | [<MainCommand>] Hello of string
+        | First_task
+        | Second_task
+        | Third_task
+        | Forth_task
+        | Fifth_task
+        | Sixth_task
         interface IArgParserTemplate with
             member s.Usage =
                 match s with
-                | Info -> "More detailed information"
-                | Version -> "Version of application"
-                | Favorite_Color _ -> "Favorite color"
-                | Hello _ -> "Who to say hello to"
-
+                | First_task -> "First task"
+                | Second_task -> "Second task"
+                | Third_task-> "Third task"
+                | Forth_task -> "Forth task"
+                | Fifth_task -> "Fifth task"
+                | Sixth_task -> "Sixth task"
+       
     [<EntryPoint>]
     let main (argv: string array) =
         let parser = ArgumentParser.Create<CLIArguments>(programName = "ProgrammingCourse")
-        let results = parser.Parse(argv)
-        if results.Contains Version then
-            AssemblyInfo.printVersion()
-        elif results.Contains Info then
-            AssemblyInfo.printInfo()
-        elif results.Contains Hello then
-            match results.TryGetResult Hello with
-            | Some v ->
-                let color = results.GetResult Favorite_Color
-                Say.hello v |> Say.colorizeIn color
-            | None -> parser.PrintUsage() |> printfn "%s"
+        try 
+        let results = parser.Parse argv
+        if results.Contains First_task then
+            printf "Введите число: "
+            let x = Console.ReadLine() |> float
+            let result = x*x*x*x+x*x*x+x*x+x+1.0
+            printf "Результат: "
+            printfn "%A" result        
+        elif results.Contains Second_task then
+            printf "Введите число: "
+            let y = Console.ReadLine() |> float
+            let q = y*y                             //x^2
+            let r = q + y                           //x^2+x
+            let result = q*r+r+1.0                  //x^2(x^2+x)+(x^2+x)+1=x^4+x^3+x^2+x+1
+            printf "Результат: "                    // 2 умножения
+            printfn "%A" result                     // и 3 сложения
+        elif results.Contains Third_task then
+            printfn "Nothing"
+        elif results.Contains Forth_task then
+            printfn "Nothing"
+        elif results.Contains Fifth_task then
+            printfn "Nothing"
+        elif results.Contains Sixth_task then
+            printfn "Nothing"
         else
             parser.PrintUsage() |> printfn "%s"
         0
+        with
+        | :? ArguParseException as ex ->
+            printfn "%s" ex.Message
+            1
+        | ex ->
+            printfn "Internal Error:"
+            printfn "%s" ex.Message
+            2
+ 
+
