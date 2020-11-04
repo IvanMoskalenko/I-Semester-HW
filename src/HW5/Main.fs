f3ab05408e4b2d5af878112ef2183d6ba51b6623
Ivan <ivon.klass@yandex.ru>
2020-11-04 10:47:27 +0300

    Added new project

---
+++ src/HW5/Main.fs
@@ -0,0 +1,12 @@
+namespace HW5
+
+module Main =
+
+    [<EntryPoint>]
+    let main (argv: string array) =
+        //let config = new PerfConfig(10_000, 20_000, 200 * 10_000, false)
+        //Lecture5.PerfTests.perfTests config List.sort 5  "timingsSystemListSortOut5_release_GC_charging_2.csv"
+        //Lecture5.PerfTests.perfTests config Sorts.qSort 5 "timingsCustomListSortOut5_release_GC_charging_2.csv"
+        0
+
+
