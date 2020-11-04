febc942e1a9f7ed4109b61518cc5cd2b927c9d86
Ivan <ivon.klass@yandex.ru>
2020-11-04 13:45:02 +0300

    F# part is done

--- src/HW5/Main.fs
+++ src/HW5/Main.fs
@@ -1,12 +1,23 @@
 namespace HW5
-
 module Main =
-
     [<EntryPoint>]
     let main (argv: string array) =
-        //let config = new PerfConfig(10_000, 20_000, 200 * 10_000, false)
-        //Lecture5.PerfTests.perfTests config List.sort 5  "timingsSystemListSortOut5_release_GC_charging_2.csv"
-        //Lecture5.PerfTests.perfTests config Sorts.qSort 5 "timingsCustomListSortOut5_release_GC_charging_2.csv"
+        let config1 = new hw5.PerfConfig (100, 200, 1500, false)
+        hw5.perfTestsForLists config1 List.sort 1  "timingsSystemListSortOut5_debug_noGC.csv"
+        hw5.perfTestsForLists config1 sorts.listQuickSort 1 "timingsCustomListQuickSortOut5_debug_noGC.csv"
+        hw5.perfTestsForLists config1 sorts.listBubbleSort 1 "timingsCustomListBubbleSortOut5_debug_noGC.csv"
+        hw5.perfTestsForArrays config1 Array.sort 1  "timingsSystemArraySortOut5_debug_noGC.csv"
+        hw5.perfTestsForArrays config1 sorts.arrayBubbleSort 1  "timingsCustomArrayBubbleSortOut5_debug_noGC.csv"
+        hw5.perfTestsForArrays config1 sorts.arrayQuickSortForExperiments 1  "timingsCustomArrayQuickSortForExperimentsOut5_debug_noGC.csv"
+        hw5.perfTestsForArrays config1 sorts.arrayQuickSortForExperiments 1  "timingsCustomArrayQuickSortOut5_debug_noGC.csv"
+        let config2 = new hw5.PerfConfig (100, 200, 1500, true)
+        hw5.perfTestsForLists config2 List.sort 1  "timingsSystemListSortOut5_debug_GC.csv"
+        hw5.perfTestsForLists config2 sorts.listQuickSort 1 "timingsCustomListQuickSortOut5_debug_GC.csv"
+        hw5.perfTestsForLists config2 sorts.listBubbleSort 1 "timingsCustomListBubbleSortOut5_debug_GC.csv"
+        hw5.perfTestsForArrays config2 Array.sort 1  "timingsSystemArraySortOut5_debug_GC.csv"
+        hw5.perfTestsForArrays config2 sorts.arrayBubbleSort 1  "timingsCustomArrayBubbleSortOut5_debug_GC.csv"
+        hw5.perfTestsForArrays config2 sorts.arrayQuickSortForExperiments 1  "timingsCustomArrayQuickSortForExperimentsOut5_debug_GC.csv"
+        hw5.perfTestsForArrays config2 sorts.arrayQuickSortForExperiments 1  "timingsCustomArrayQuickSortOut5_debug_GC.csv"
         0
 
 
---
+++ src/HW5/hw5.fs
@@ -0,0 +1,60 @@
+namespace HW5
+module hw5 =
+    open System
+
+    [<Struct>]
+    type PerfConfig =
+        val From: int
+        val Step: int
+        val To  : int
+        val ForceGC: bool
+        new (_from, _step, _to, forceGC) = {From = _from; Step = _step; To = _to; ForceGC = forceGC}
+
+    let genRandomList n =
+        let rand = new System.Random()
+        List.init n (fun _ -> rand.Next())
+
+    let genRandomArray n =
+        let rand = new System.Random()
+        Array.init n (fun _ -> rand.Next())
+
+    let time f =
+        let timer = new System.Diagnostics.Stopwatch()
+        timer.Start()
+        let res = f()
+        let time = timer.ElapsedMilliseconds
+        res,time
+
+    let perfTestsForLists (perfConfig: PerfConfig) sortFun sycles file =
+        let steps = [perfConfig.From .. perfConfig.Step .. perfConfig.To]
+        let timings = Array.zeroCreate steps.Length
+        let mutable j = 0
+        for i in steps do
+            let lst = genRandomList i
+            let times = Array.zeroCreate sycles
+            for j in 0 .. sycles - 1 do
+                if perfConfig.ForceGC then GC.Collect()
+                let res, time = time (fun _ -> sortFun lst)
+                if perfConfig.ForceGC then GC.Collect()
+                times.[j] <- string time
+                printfn "Measured: len = %A, iteration = %A, time = %A" i j time
+            timings.[j] <- (string i) + ","  + (String.concat ", " times)
+            j <- j + 1
+        System.IO.File.WriteAllLines (file, timings)
+
+    let perfTestsForArrays (perfConfig: PerfConfig) sortFun sycles file =
+        let steps = [perfConfig.From .. perfConfig.Step .. perfConfig.To]
+        let timings = Array.zeroCreate steps.Length
+        let mutable j = 0
+        for i in steps do
+            let lst = genRandomArray i
+            let times = Array.zeroCreate sycles
+            for j in 0 .. sycles - 1 do
+                if perfConfig.ForceGC then GC.Collect()
+                let res, time = time (fun _ -> sortFun lst)
+                if perfConfig.ForceGC then GC.Collect()
+                times.[j] <- string time
+                printfn "Measured: len = %A, iteration = %A, time = %A" i j time
+            timings.[j] <- (string i) + ","  + (String.concat ", " times)
+            j <- j + 1
+        System.IO.File.WriteAllLines (file, timings)
