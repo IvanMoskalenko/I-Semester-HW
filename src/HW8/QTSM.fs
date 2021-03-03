d930ab63ad3bcef23f468c50b77546811171244d
Ivan <ivon.klass@yandex.ru>
2021-03-03 03:29:05 +0300

    Sum, multiply & tensor multiply

---
+++ src/HW8/QTSM.fs
@@ -0,0 +1,142 @@
+module QTSM
+
+type QuadTree<'t> =
+    | None
+    | Leaf of 't
+    | Node of QuadTree<'t> * QuadTree<'t> * QuadTree<'t> * QuadTree<'t>
+       
+type SparseMatrix<'t> =
+    val numOfRows: int
+    val numOfCols : int
+    val notEmptyCells: list<int * int * 't>
+    new (x, y, lst) = {numOfRows = x; numOfCols = y; notEmptyCells = lst}
+    
+let inline sum (x: QuadTree<'t>) (y: QuadTree<'t>) =
+    let rec _go (x: QuadTree<'t>) (y: QuadTree<'t>) =
+        match x, y with
+        | Leaf a, Leaf b -> Leaf (a + b)
+        | None, k -> k
+        | k, None -> k
+        | Node (x1, x2, x3, x4), Node (y1, y2, y3, y4) ->
+            let NW = _go x1 y1
+            let NE = _go x2 y2
+            let SW = _go x3 y3
+            let SE = _go x4 y4
+            if NW = None && NE = None && SW = None && SE = None
+            then None
+            else Node (NW, NE, SW, SE)
+        | _, _ -> failwith "It's impossible to sum this"
+    _go x y
+    
+let inline multiply (x: QuadTree<'t>) (y: QuadTree<'t>) =
+    let rec _go (x: QuadTree<'t>) (y: QuadTree<'t>) =
+        match x, y with
+        | Leaf a, Leaf b -> Leaf (a * b)
+        | None, _ -> None
+        | _, None -> None
+        | Node (x1, x2, x3, x4), Node (y1, y2, y3, y4) ->
+            let NW = sum (_go x1 y1) (_go x2 y3)
+            let NE = sum (_go x1 y2) (_go x2 y4)
+            let SW = sum (_go x3 y1) (_go x4 y3)
+            let SE = sum (_go x3 y2) (_go x4 y4)
+            if NW = None && NE = None && SW = None && SE = None
+            then None
+            else Node (NW, NE, SW, SE)
+        | _, _ -> failwith "It's impossible to multiply this"
+    _go x y
+    
+let inline scalarMultiply (x: QuadTree<'t>) (scalar: 't) =
+    let rec _go (x: QuadTree<'t>) =
+        match x with
+        | Leaf t -> Leaf (t * scalar)
+        | None -> None
+        | Node (NW, NE, SW, SE) -> Node (_go NW, _go NE, _go SW, _go SE)
+    _go x
+    
+let inline tensorMultiply (x: QuadTree<'t>) (y: QuadTree<'t>) =
+    if x = None || y = None then None
+    else
+        let rec _go (x: QuadTree<'t>) =
+            match x with
+            | Leaf t -> scalarMultiply y t
+            | None -> None
+            | Node (NW, NE, SW, SE) -> Node (_go NW, _go NE, _go SW, _go SE)
+        _go x
+
+let toMatrix quadTree size =
+    
+    let output = Array2D.create size size 0    
+    let rec _go quadTree (x, y) size =
+        let hSize = size / 2
+        let qSize = size / 4
+        match quadTree, size with
+        | Leaf t, _ -> output.[x, y] <- t
+        | None, _ -> output.[0, 0] <- output.[0, 0]
+        | Node (q1, q2, q3, q4), 2 ->
+            let nwQP = x - hSize, y - hSize
+            let neQP = x - hSize, y
+            let swQP = x, y - hSize
+            let seQP = x, y
+            _go q1 nwQP hSize; _go q2 neQP hSize; _go q3 swQP hSize; _go q4 seQP hSize
+        | Node (q1, q2, q3, q4), _ ->
+            let nwQP = x - qSize, y - qSize
+            let neQP = x - qSize, y + qSize
+            let swQP = x + qSize, y - qSize
+            let seQP = x + qSize, y + qSize
+            _go q1 nwQP hSize; _go q2 neQP hSize; _go q3 swQP hSize; _go q4 seQP hSize
+    _go quadTree (size / 2, size / 2) size
+    
+    let toSparse matrix =
+        let mutable k = 0
+        for i in 0 .. Array2D.length1 matrix - 1 do
+            for j in 0 .. Array2D.length2 matrix - 1 do
+                if matrix.[i,j] <> 0
+                then k <- k + 1
+        let unrealIndex = Array2D.length1 matrix + 1
+        let output = Array.create k (unrealIndex, unrealIndex, 0)
+        k <- 0
+        for i in 0 .. Array2D.length1 matrix - 1 do
+            for j in 0 .. Array2D.length2 matrix - 1 do
+                if matrix.[i,j] <> 0
+                then
+                    output.[k] <- i, j, matrix.[i,j]
+                    k <- k + 1
+        SparseMatrix(unrealIndex - 1, unrealIndex - 1, List.ofArray output)        
+    toSparse output
+    
+let first (x, _, _) = x
+let second (_, x, _) = x
+let third (_, _, x) = x
+let toPowerOf2 x =
+        let rec go r = if pown 2 r < x then go (r + 1) else r
+        pown 2 (go 0)
+        
+let toTree matrix =      
+    
+    let toPowOf2SizedMatrix (matrix: SparseMatrix<'t>) =
+        let max = max matrix.numOfRows matrix.numOfCols
+        SparseMatrix (toPowerOf2 max, toPowerOf2 max, matrix.notEmptyCells)            
+            
+    let divTo4 (matrix: SparseMatrix<'t>) =
+        let l1 = List.filter (fun (i, j, _) -> (i + 1 <= (matrix.numOfRows / 2) && j + 1 <= (matrix.numOfCols / 2))) matrix.notEmptyCells
+        let q1 = new SparseMatrix<'t>((matrix.numOfRows) / 2, (matrix.numOfCols)/2, l1)
+        let l2 = List.filter (fun (i, j, _) -> (i + 1 <= (matrix.numOfRows / 2) && j + 1 > (matrix.numOfCols / 2))) matrix.notEmptyCells
+        let l2n = List.map (fun (i, j, k) -> i, j - matrix.numOfCols/2, k) l2
+        let q2 = new SparseMatrix<'t>((matrix.numOfRows) / 2, (matrix.numOfCols)/2, l2n)
+        let l3 = List.filter (fun (i, j, _) -> (i + 1 > (matrix.numOfRows/2) && j + 1 <= (matrix.numOfCols / 2))) matrix.notEmptyCells
+        let l3n = List.map (fun (i, j, k) -> i - matrix.numOfRows/2, j, k) l3
+        let q3 = new SparseMatrix<'t>((matrix.numOfRows) / 2, (matrix.numOfCols)/2, l3n)
+        let l4 = List.filter (fun (i, j, _) -> (i + 1 > (matrix.numOfRows / 2) && j + 1 > (matrix.numOfCols / 2))) matrix.notEmptyCells
+        let l4n = List.map (fun (i, j, k) -> i - matrix.numOfRows / 2, j - matrix.numOfCols / 2, k) l4
+        let q4 = new SparseMatrix<'t>((matrix.numOfRows) / 2, (matrix.numOfCols) / 2, l4n)
+        (q1, q2, q3, q4)
+            
+    let rec f (matrix: SparseMatrix<'t>) =
+        if List.isEmpty matrix.notEmptyCells then QuadTree.None
+        elif matrix.numOfRows = 1 && matrix.numOfCols = 1
+        then QuadTree.Leaf <| (matrix.notEmptyCells.Head |> third)
+        else
+            let (q1, q2, q3, q4) = divTo4 matrix
+            QuadTree.Node (f q1, f q2, f q3, f q4)
+    
+    f (toPowOf2SizedMatrix matrix)
--- tests/HW8.Tests/Tests.fs
+++ tests/HW8.Tests/Tests.fs
@@ -1,3 +1,86 @@
 module Tests
+open System
 open Expecto
+open HW2_3
+open QTSM
+open hw3
+let generateSparseMatrix rows cols =
+    let x = Array2D.init rows cols (fun _ _ -> Random().Next(0, 2))
+    SparseMatrix (rows, cols,
+                            [for i in 0..rows - 1 do
+                                 for j in 0..cols - 1 do
+                                     if x.[i, j] = 1 then (i, j, Random().Next(1, 100))])    
+let genArrayBySparseMatrix (matrix: SparseMatrix<int>) =
+    let output = Array2D.zeroCreate matrix.numOfRows matrix.numOfCols
+    for x in matrix.notEmptyCells do
+        output.[first x, second x] <- third x
+    output
 
+let standartSum (x: int[,]) (y: int[,]) =
+    if Array2D.length1 x <> Array2D.length1 y || Array2D.length2 x <> Array2D.length2 y then failwith "Dimensions of matrices should be equal"
+    else
+        for i in 0 .. Array2D.length1 x - 1 do
+            for j in 0 .. Array2D.length2 x - 1 do
+                x.[i, j] <- x.[i, j] + y.[i, j]
+    x
+
+let standartMultiplyByScalar scalar (x: int[,]) =
+    let y = Array2D.copy x
+    for i in 0 .. Array2D.length1 x - 1 do
+        for j in 0 .. Array2D.length2 x - 1 do
+            y.[i, j] <- scalar * y.[i, j]
+    y
+let standartTensorMultiply (x: int[,]) (y: int[,]) =
+    let mutable a, b = 0, 0
+    let output = Array2D.create (Array2D.length1 x * Array2D.length2 y) (Array2D.length1 x * Array2D.length2 y) 1
+    for i in 0 .. Array2D.length1 x - 1 do
+        for j in 0 .. Array2D.length2 x - 1 do
+            a <- i * (Array2D.length1 y) 
+            b <- j * (Array2D.length2 y)
+            Array2D.blit (standartMultiplyByScalar x.[i,j] y) 0 0 output a b (Array2D.length1 x) (Array2D.length2 y)
+    output
+    
+let arrayToSparseMatrix (x: int[,]) =
+    let y = Array.create (Array2D.length1 x * Array2D.length2 x) (0, 0, 0)
+    let mutable z = 0
+    for i in 0 .. Array2D.length1 x - 1 do
+        for j in 0 .. Array2D.length2 x - 1 do
+            y.[z] <- i, j, x.[i,j]
+            z <- z + 1
+    SparseMatrix(Array2D.length1 x, Array2D.length2 x, y |> Array.filter (fun x -> (x |> third) <> 0) |> Array.toList)
+
+[<Tests>]
+let tests =
+    testList "Tests for QuadTree" [    
+        testProperty "Sum" <| fun _ ->
+                    let dim = Random().Next(0, 64)
+                    let sm1 = generateSparseMatrix dim dim
+                    let sm2 = generateSparseMatrix dim dim
+                    let m1 = genArrayBySparseMatrix sm1
+                    let m2 = genArrayBySparseMatrix sm2
+                    let sum1 = standartSum m1 m2 |> arrayToSparseMatrix |> toTree
+                    let sum2 = sum (sm1 |> toTree) (sm2 |> toTree)
+                    Expect.equal sum1 sum2 ""
+                    
+        testProperty "Mul" <| fun _ ->
+                    let dim = Random().Next(0, 64)
+                    let sm1 = generateSparseMatrix dim dim
+                    let sm2 = generateSparseMatrix dim dim
+                    let m1 = genArrayBySparseMatrix sm1
+                    let m2 = genArrayBySparseMatrix sm2
+                    let mul1 = matrixMultiply m1 m2 |> arrayToSparseMatrix |> toTree
+                    let mul2 = multiply (sm1 |> toTree) (sm2 |> toTree)
+                    Expect.equal mul1 mul2 ""
+                    
+        testProperty "Tensor mul" <| fun _ ->
+                    let dim = Random().Next(0, 64) |> toPowerOf2
+                    let sm1 = generateSparseMatrix dim dim
+                    let sm2 = generateSparseMatrix dim dim
+                    let m1 = genArrayBySparseMatrix sm1
+                    let m2 = genArrayBySparseMatrix sm2
+                    printfn "%A" m1
+                    printfn "%A" m2
+                    printfn "%A" (standartTensorMultiply m1 m2)
+                    let tmul1 = standartTensorMultiply m1 m2 |> arrayToSparseMatrix |> toTree
+                    let tmul2 = tensorMultiply (sm1 |> toTree) (sm2 |> toTree)
+                    Expect.equal tmul1 tmul2 ""]
