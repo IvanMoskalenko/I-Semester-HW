95e494d3fb2858fecc8604d6bc9065041259230b
Ivan <ivon.klass@yandex.ru>
2021-04-15 18:50:06 +0300

    v 1.0

---
+++ src/Generator/Generator.fs
@@ -0,0 +1,41 @@
+module Generator
+open System
+
+let printMatrix (x: string [,]) path =
+    let y = x.[*, 1]
+    let z = x.[1, *]
+    let mutable text = ""
+    for i = 0 to y.Length - 1 do
+        for j = 0 to z.Length - 1 do
+            text <- text + x.[i, j] + " "
+        text <- text + "\n"
+    System.IO.File.AppendAllText (path, text)
+                
+let generator rows cols amt sparsity path bType =  
+    for i = 0 to amt - 1 do
+        let output = Array2D.zeroCreate rows cols
+        for j = 0 to rows - 1 do
+            for k = 0 to cols - 1 do
+                let x = Random()
+                let y = (x.Next (100) |> float) / 100.0
+                if bType = "int" then
+                    if y > sparsity   
+                    then output.[j, k] <- string (x.Next (100))
+                    else output.[j, k] <- "0"
+                elif bType = "float" then
+                    if y > sparsity   
+                    then output.[j, k] <- string ((x.Next (1000) |> float) / 10.0) 
+                    else output.[j, k] <- "0"
+                elif bType = "bool" then
+                    if y > sparsity  
+                    then output.[j, k] <- "1" 
+                    else output.[j, k] <- "0"
+                else failwith "Your type is unsupported"
+        printMatrix output (path + "/Matrix" + string i + ".txt")
+        
+let first (x, _, _, _, _, _) = x
+let second (_, x, _, _, _, _) = x
+let third (_, _, x, _, _, _) = x
+let fourth (_, _, _, x, _, _) = x
+let fifth (_, _, _, _, x, _) = x
+let sixth (_, _, _, _, _, x) = x
---
+++ src/Generator/Main.fs
@@ -0,0 +1,36 @@
+﻿module Main
+open Generator
+open Argu
+
+type CliArguments =
+    | Generator of rows: int * cols: int * amount: int * sparsity: float * path: string * bType: string
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Generator _ -> "specify a working directory."
+
+[<EntryPoint>]
+    let main (argv: string array) =
+        let parser = ArgumentParser.Create<CliArguments>(programName = "Generator")
+        try
+        let results = parser.Parse argv
+        if results.Contains Generator then
+            let x = results.GetResult Generator
+            let rows = x |> first
+            let cols = x |> second
+            let amount = x |> third
+            let sparsity = x |> fourth
+            let path = x |> fifth
+            let bType = x |> sixth
+            generator rows cols amount sparsity path bType   
+        else
+            parser.PrintUsage() |> printfn "%s"
+        0    
+        with
+        | :? ArguParseException as ex ->
+            printfn "%s" ex.Message
+            1
+        | ex ->
+            printfn "Internal Error:"
+            printfn "%s" ex.Message
+            2     
